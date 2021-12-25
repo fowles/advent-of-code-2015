d506a00d3102a44b80993b1506d89cefe272d899
Matt Kulukundis <matt.fowles@gmail.com>
2021-12-25 16:14:22 -0500

    Day 2 part 1
    Most of the time was spenting figuring out how to inject basic parsing
    functions into String.

--- day2.swift
+++ day2.swift
@@ -1,8 +1,13 @@
 import Foundation;
 
 struct Day2 {
-static func part1(_ raw:String) -> Int {
-  return 0;
+static func part1(_ dims:[(Int, Int, Int)]) -> Int {
+  var res = 0
+  for (h, w, l) in dims {
+      res += 2*l*w + 2*w*h + 2*h*l
+      res += min(l*w, w*h, h*l)
+  }
+  return res
 }
 
 static func part2(_ raw:String) -> Int {
@@ -11,8 +16,11 @@ static func part2(_ raw:String) -> Int {
 
 static func main() throws {
   let raw = try String(contentsOfFile:"input/day2.txt")
+  let lines = try raw.split(separator:"\n").map {
+     try $0.extract(regex:#"(\d+)"#).map({ Int($0)! })
+  }
 
-  print("Day 2 part 1:", part1(raw));
-  print("Day 2 part 2:", part2(raw));
+  print("Day 2 part 1:", part1(lines.map { $0.splat() }))
+  print("Day 2 part 2:", part2(raw))
 }
 }
--- main.swift
+++ main.swift
@@ -1,8 +1,44 @@
+import Foundation
 import Darwin
+
+extension Array {
+  func splat() -> (Element,Element) {
+    return (self[0],self[1])
+  }
+
+  func splat() -> (Element,Element,Element) {
+    return (self[0],self[1],self[2])
+  }
+
+  func splat() -> (Element,Element,Element,Element) {
+    return (self[0],self[1],self[2],self[3])
+  }
+
+  func splat() -> (Element,Element,Element,Element,Element) {
+    return (self[0],self[1],self[2],self[3],self[4])
+  }
+}
+
+public extension String.SubSequence {
+  func extract(regex: String) throws -> [String.SubSequence] {
+    let r = try NSRegularExpression(pattern: regex, options: [])
+    let matches = r.matches(in:String(self), options:[], range:NSMakeRange(0, self.count))
+
+    var res: [String.SubSequence] = []
+    for m in matches {
+      for i in 1..<m.numberOfRanges {
+        if let range = Range(m.range(at:i), in:self) {
+          res.append(self[range])
+        }
+      }
+    }
+    return res;
+  }
+}
 
 do {
   chdir(getenv("BUILD_WORKING_DIRECTORY"))
-  try Day1.main()
+  try Day2.main()
 } catch {
   print("Error: \(error).")
 }

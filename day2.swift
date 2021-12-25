2c0781dc509d88518c57351b9c0f978fff5457ef
Matt Kulukundis <matt.fowles@gmail.com>
2021-12-25 16:19:56 -0500

    Day 2 part 2
    simple arithematic

--- day2.swift
+++ day2.swift
@@ -10,17 +10,22 @@ static func part1(_ dims:[(Int, Int, Int)]) -> Int {
   return res
 }
 
-static func part2(_ raw:String) -> Int {
-  return 0;
+static func part2(_ dims:[(Int, Int, Int)]) -> Int {
+  var res = 0
+  for (h, w, l) in dims {
+      res += h*w*l
+      res += min(2*(l+w), 2*(w+h), 2*(h+l))
+  }
+  return res
 }
 
 static func main() throws {
   let raw = try String(contentsOfFile:"input/day2.txt")
-  let lines = try raw.split(separator:"\n").map {
-     try $0.extract(regex:#"(\d+)"#).map({ Int($0)! })
+  let lines: [(Int, Int, Int)] = try raw.split(separator:"\n").map {
+     try $0.extract(regex:#"(\d+)"#).map({ Int($0)! }).splat()
   }
 
-  print("Day 2 part 1:", part1(lines.map { $0.splat() }))
-  print("Day 2 part 2:", part2(raw))
+  print("Day 2 part 1:", part1(lines))
+  print("Day 2 part 2:", part2(lines))
 }
 }

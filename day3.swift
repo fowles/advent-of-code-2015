c10bf935cee4c7352e6fe1568d91faa5dc967682
Matt Kulukundis <matt.fowles@gmail.com>
2021-12-25 17:12:46 -0500

    Day part 2
    Lack of bool to int conversion is probably a feature

--- day3.swift
+++ day3.swift
@@ -35,7 +35,22 @@ static func part1(_ raw:String) -> Int {
 }
 
 static func part2(_ raw:String) -> Int {
-  return 0;
+  var visited: Set<Point> = []
+  var pos = [Point(x:0, y:0), Point(x:0, y:0)]
+  visited.insert(pos[0])
+  var turn = 0
+  for c in raw {
+      switch c {
+          case "^": pos[turn].y += 1
+          case "v": pos[turn].y -= 1
+          case "<": pos[turn].x -= 1
+          case ">": pos[turn].x += 1
+          default: ()
+      }
+      visited.insert(pos[turn])
+      turn = turn == 0 ? 1 : 0
+  }
+  return visited.count
 }
 
 static func main() throws {

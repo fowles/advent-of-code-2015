212647cd4f3ac5c977f2c68dce266349d83bd1cc
Matt Kulukundis <matt.fowles@gmail.com>
2021-12-25 23:29:34 -0500

    Day 5 part 1
    Simple rules.  Starting to get the hang of swift.

--- day5.swift
+++ day5.swift
@@ -1,16 +1,52 @@
 import Foundation;
 
-struct Day5 {
-static func part1(_ raw:String) -> Int {
-  return 0;
+private func isNice(_ l:String.SubSequence) -> Bool {
+  if (l.contains("ab") ||
+      l.contains("cd") ||
+      l.contains("pq") ||
+      l.contains("xy")) {
+    return false;
+  }
+
+  var dup = false
+  var vowels = 0
+  var last: Character = "."
+  for c in l {
+    if last == c {
+      dup  = true
+    }
+    last = c
+
+    switch c {
+      case "a": vowels += 1
+      case "e": vowels += 1
+      case "i": vowels += 1
+      case "o": vowels += 1
+      case "u": vowels += 1
+      default: ()
+    }
+  }
+  return dup && vowels > 2;
 }
 
-static func part2(_ raw:String) -> Int {
+struct Day5 {
+static func part1(_ lines:[String.SubSequence]) -> Int {
+  var nice = 0;
+  for l in lines {
+    if (isNice(l)) {
+      nice += 1
+    }
+  }
+  return nice;
+}
+
+static func part2(_ raw:[String.SubSequence]) -> Int {
   return 0;
 }
 
 static func main() throws {
   let raw = try String(contentsOfFile:"input/day5.txt")
+      .split(separator:"\n")
 
   print("Day 5 part 1:", part1(raw));
   print("Day 5 part 2:", part2(raw));
--- main.swift
+++ main.swift
@@ -38,7 +38,7 @@ public extension String.SubSequence {
 
 do {
   chdir(getenv("BUILD_WORKING_DIRECTORY"))
-  try Day4.main()
+  try Day5.main()
 } catch {
   print("Error: \(error).")
 }
